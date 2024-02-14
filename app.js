2ae2ce52ed055127268176edaf6aed19e1c25ff0
velasquez1606 <sebsv16@gmail.com>
2024-02-13 21:30:48 -0500

    update file app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function encriptar() {
     let wordEncript = document.getElementById('wordEncript').value;
     let value = document.getElementById('text');
     let result = '';
+    // Con la variable wordEncript iteramos sobre esta y cuando llegamos a las vocales
+    // cambiamos estas por su valor encriptado siendo result el encriptado
     for (let i = 0; i <= wordEncript.length-1; i++) {
         if (wordEncript[i] == 'a') {
             result += 'ai';
@@ -28,6 +30,10 @@ function desencriptar(){
     let value = document.getElementById('text');
     let result = '';
     let iter = 0;
+    // Con wordEncript iteramos sobre las vocales, como estas estan encriptadas,
+    // solo tomamos su caracter inicial, por ejemplo "ufat" solo toamos u
+    // e incertamos los caracteres en result, con iter adelantamos la iteracion para saltarnos
+    // el valor encriptado
     for (let i=0; i <= wordEncript.length-1; i+=iter) {
         if (wordEncript[i] == 'a') {
             iter = 2;
