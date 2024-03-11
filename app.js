36183ab95d67b83a29b203de7cbe821c0a6b44ad
velasquez1606 <sebsv16@gmail.com>
2024-03-11 13:51:09 -0500

    update

--- app.js
+++ app.js
@@ -1,54 +1,68 @@
-encriptar();
-desencriptar();
+const wordEncript = document.querySelector('.word-encript');
+const text = document.querySelector('.mensaje');
 
-function encriptar() {
-    let wordEncript = document.getElementById('wordEncript').value;
-    let value = document.getElementById('text');
+function encriptar(wordEncript2) {
     let result = '';
-    // Con la variable wordEncript iteramos sobre esta y cuando llegamos a las vocales
+    // Con la variable wordEncript2 iteramos sobre esta y cuando llegamos a las vocales
     // cambiamos estas por su valor encriptado siendo result el encriptado
-    for (let i = 0; i <= wordEncript.length-1; i++) {
-        if (wordEncript[i] == 'a') {
+    for (let i = 0; i <= wordEncript2.length-1; i++) {
+        if (wordEncript2[i] == 'a') {
             result += 'ai';
-        } else if (wordEncript[i] == 'e') {
+        } else if (wordEncript2[i] == 'e') {
             result += 'enter';
-        } else if (wordEncript[i] == 'i') {
+        } else if (wordEncript2[i] == 'i') {
             result += 'imes';
-        } else if (wordEncript[i] == 'o') {
+        } else if (wordEncript2[i] == 'o') {
             result += 'ober';
-        } else if (wordEncript[i] == 'u') {
+        } else if (wordEncript2[i] == 'u') {
             result += 'ufat';
         } else {
-            result += wordEncript[i];
+            result += wordEncript2[i];
         }
     }
-    value.innerHTML = result;
+    //value.innerHTML = result;
+    return result;
 }
 
-function desencriptar(){
-    let wordEncript = document.getElementById('wordEncript').value;
-    let value = document.getElementById('text');
+function desencriptar(wordEncript2){
     let result = '';
     let iter = 0;
-    // Con wordEncript iteramos sobre las vocales, como estas estan encriptadas,
+    // Con wordEncript2 iteramos sobre las vocales, como estas estan encriptadas,
     // solo tomamos su caracter inicial, por ejemplo "ufat" solo toamos u
     // e incertamos los caracteres en result, con iter adelantamos la iteracion para saltarnos
     // el valor encriptado
-    for (let i=0; i <= wordEncript.length-1; i+=iter) {
-        if (wordEncript[i] == 'a') {
+    for (let i=0; i <= wordEncript2.length-1; i+=iter) {
+        if (wordEncript2[i] == 'a') {
             iter = 2;
-        } else if (wordEncript[i] == 'e') {
+        } else if (wordEncript2[i] == 'e') {
             iter = 5;
-        } else if (wordEncript[i] == 'i') {
+        } else if (wordEncript2[i] == 'i') {
             iter = 4;
-        } else if (wordEncript[i] == 'o') {
+        } else if (wordEncript2[i] == 'o') {
             iter = 4;
-        } else if (wordEncript[i] == 'u') {
+        } else if (wordEncript2[i] == 'u') {
             iter = 4;
         } else {
             iter = 1;
         }
-        result += wordEncript[i];
+        result += wordEncript2[i];
     }
-    value.innerHTML = result;
+    //value.innerHTML = result;
+    return result;
+}
+
+function boton_encriptar(){
+    text.value = encriptar(wordEncript.value);
+    wordEncript.value = '';
+}
+
+function boton_desencriptar(){
+    text.value = desencriptar(wordEncript.value);
+    wordEncript.value = '';
+}
+
+function copiar(){
+    text.select();
+    navigator.clipboard.writeText(mensaje.value)
+    alert("Texto Copiado")
 }
